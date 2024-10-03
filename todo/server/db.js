6a805301b3ca28e4ed938f51058cddd6f4b63297
vishnuvichu012345 <vuvishnu23@gmail.com>
2024-10-03 22:38:42 +0530

    Deployement-configaration

--- todo/server/db.js
+++ todo/server/db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 // Initialize Sequelize
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
 });
 

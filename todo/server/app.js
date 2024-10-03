621514c954f114061d17345ffe2dfcc64222a26a
vishnuvichu012345 <vuvishnu23@gmail.com>
2024-10-04 01:24:05 +0530

    googleLogin

--- todo/server/app.js
+++ todo/server/app.js
@@ -1,3 +1,4 @@
+// app.js
 const express = require('express');
 const { connectDB } = require('./db');
 const { User } = require('./models');
@@ -6,17 +7,11 @@ const taskRoutes = require('./routes/tasks');
 const cors = require('cors');
 require('dotenv').config();
 
+
 const app = express();
 
-// Set the CORS options
-const corsOptions = {
-  origin: 'https://vooshfoods-todo.vercel.app', // Replace with your actual frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-  credentials: true, // Enable cookies and authorization headers
-};
 
-// Use CORS with the specified options
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Connect to MySQL
 connectDB();
@@ -28,6 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
